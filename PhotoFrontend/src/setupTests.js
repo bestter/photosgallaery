bd9ebf0e6a69162a2bff63627d2a34c0bf9800e6
Martin Labelle <martin.labelle1@gmail.com>
2026-05-12 10:21:47 -0400

    Fix tests

--- PhotoFrontend/src/setupTests.js
+++ PhotoFrontend/src/setupTests.js
@@ -1,1 +1,39 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+const localStorageMock = (function () {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    }
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+});
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  }
+}));
