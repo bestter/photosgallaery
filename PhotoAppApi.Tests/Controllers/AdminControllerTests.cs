d8c0c56459c0802f82ad565a715c51fabe9c6fec
Martin Labelle <martin.labelle1@gmail.com>
2026-07-23 11:06:25 -0400

    Update AdminControllerTests.cs
    Remove not used suing

--- PhotoAppApi.Tests/Controllers/AdminControllerTests.cs
+++ PhotoAppApi.Tests/Controllers/AdminControllerTests.cs
@@ -1,20 +1,13 @@
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
 using PhotoAppApi.Controllers;
 using PhotoAppApi.Data;
 using PhotoAppApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace PhotoAppApi.Tests.Controllers
 {
